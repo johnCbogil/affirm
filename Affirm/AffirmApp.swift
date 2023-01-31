2b906bb95b25edd5f7fa9f1e31b4182cc14e53bc
John Bogil <johncbogil@gmail.com>
2023-01-31 18:50:12 -0500

    Initial Commit

---
+++ Affirm/AffirmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AffirmApp.swift
+//  Affirm
+//
+//  Created by John Bogil on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct AffirmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
