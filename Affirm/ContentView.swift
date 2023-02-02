1d406495ece02846f1cf66cdc0338d7388e7331c
John Bogil <johncbogil@gmail.com>
2023-02-01 19:00:49 -0500

    initial commit

--- Affirm/ContentView.swift
+++ Affirm/ContentView.swift
@@ -6,16 +6,62 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
+    private let affirmationText = ["one", "two", "three"]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All set!")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+            
+            Spacer()
+
+            Button("Schedule Notification") {
+                generateHourlyNotification()
+            }
+            Spacer()
         }
-        .padding()
+    }
+    
+    private func generateNotificationRequests() {
+        var timeInterval = 0
+        
+        for text in affirmationText {
+            timeInterval += 1
+            let content = UNMutableNotificationContent()
+            content.title = text
+            content.sound = UNNotificationSound.default
+            
+            var date = DateComponents()
+            date.minute = 1
+            let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
+            
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            UNUserNotificationCenter.current().add(request)
+        }
+    }
+    
+    private func generateHourlyNotification() {
+        let center = UNUserNotificationCenter.current()
+        let content = UNMutableNotificationContent()
+        content.title = "Hourly Notification"
+        content.body = "This is an hourly notification"
+
+        var date = DateComponents()
+        date.minute = 1
+        let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
+
+        let request = UNNotificationRequest(identifier: "hourly_notification", content: content, trigger: trigger)
+        center.add(request)
     }
 }
 
