d633b4a32212aea954071aacbc43aa497ce03ded
John Bogil <johncbogil@gmail.com>
2023-02-03 12:28:55 -0500

    Background scheduling should be working

--- Affirm/AffirmApp.swift
+++ Affirm/AffirmApp.swift
@@ -6,12 +6,65 @@
 //
 
 import SwiftUI
+import BackgroundTasks
+import UserNotifications
 
 @main
 struct AffirmApp: App {
+    @Environment(\.scenePhase) private var phase
+    private let scheduleAffirmationsIdentifier = "myapprefresh"
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .onChange(of: phase) { newPhase in
+            switch newPhase {
+            case .active: scheduleAffirmations()
+            case .background: scheduleAppRefresh()
+            default: break
+            }
+        }
+        .backgroundTask(.appRefresh(scheduleAffirmationsIdentifier)) {
+            // NOISE
+            scheduleAffirmations()
+        }
+    }
+    
+    private func scheduleAffirmations() {
+        var affirmationText = ["one", "two", "three"]
+        let center = UNUserNotificationCenter.current()
+        center.removeAllPendingNotificationRequests()
+        
+        let hours = generateHours()
+        
+        for i in 0...2 {
+            let content = UNMutableNotificationContent()
+            content.title = affirmationText.randomElement()!
+            affirmationText.removeAll(where: { $0 == content.title })
+            
+            var date = DateComponents()
+            date.hour = hours[i]
+            date.minute = Int.random(in: 0...59)
+            
+            let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: false)
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            center.add(request)
+            print(request)
+        }
+    }
+    
+    private func generateHours() -> [Int] {
+        var output = [Int]()
+        output.append(Int.random(in: 8...12))
+        output.append(Int.random(in: 13...17))
+        output.append(Int.random(in: 18...22))
+        return output
+    }
+    
+    private func scheduleAppRefresh() {
+        let request = BGAppRefreshTaskRequest(identifier: scheduleAffirmationsIdentifier)
+        request.earliestBeginDate = Date(timeIntervalSinceNow: 24 * 60 * 60) // 24 hours from now
+        try? BGTaskScheduler.shared.submit(request)
     }
 }
--- Affirm/ContentView.swift
+++ Affirm/ContentView.swift
@@ -11,7 +11,6 @@ import UserNotifications
 struct ContentView: View {
     var body: some View {
         VStack {
-            Spacer()
             Button("Request Permission") {
                 UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
                     if success {
@@ -21,45 +20,7 @@ struct ContentView: View {
                     }
                 }
             }
-            
-            Spacer()
-
-            Button("Schedule Affirmations") {
-                scheduleAffirmations()
-            }
-            Spacer()
         }
-    }
-    
-    private func scheduleAffirmations() {
-        var affirmationText = ["one", "two", "three"]
-        let center = UNUserNotificationCenter.current()
-        center.removeAllPendingNotificationRequests()
-        
-        let hours = generateHours()
-        
-        for i in 0...2 {
-            let content = UNMutableNotificationContent()
-            content.title = affirmationText.randomElement()!
-            affirmationText.removeAll(where: { $0 == content.title })
-            
-            var date = DateComponents()
-            date.hour = hours[i]
-            date.minute = Int.random(in: 0...59)
-            
-            let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
-            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-            center.add(request)
-            print(request)
-        }
-    }
-    
-    private func generateHours() -> [Int] {
-        var output = [Int]()
-        output.append(Int.random(in: 8...12))
-        output.append(Int.random(in: 13...17))
-        output.append(Int.random(in: 18...22))
-        return output
     }
 }
 
